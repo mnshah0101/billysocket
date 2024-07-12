f5578f0093d59c36efa0087d04a252e5efd8c921
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-07-11 22:41:22 -0400

    error handling

--- utils/executor.py
+++ utils/executor.py
@@ -3,9 +3,48 @@ import dotenv
 import os
 import psycopg2
 
+from langchain.agents import initialize_agent, AgentExecutor
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+import time
+from langchain_anthropic import ChatAnthropic
+
 dotenv.load_dotenv()
 
+
+
 DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+prompt_template = """
+
+There was an error executing this sql query.
+
+{query}
+
+
+This was the error message: {error_message}
+
+Give me the fixed query to run. Do not include any other information, I want to see the fixed query only.
+
+"""
+
+prompt_template = PromptTemplate.from_template(prompt_template)
+
+def new_sql_query(old_query, error_message, model):
+
+    llm = None
+    if model == 'openai':
+        llm = ChatOpenAI(model='gpt-4o')
+
+    elif model == 'anthropic':
+        llm = ChatAnthropic(model_name='claude-3-5-sonnet-20240620')
+
+    llm_chain = prompt_template | llm
+    answer = llm_chain.invoke({'query': old_query, 'error_message': error_message})
+
+    return answer.content
+
 
 
 def execute_query(query):
@@ -20,8 +59,9 @@ def execute_query(query):
     try:
         cur.execute(query)
     except Exception as e:
-        print(f'Error: {e}')
-        return "There was an error executing the query."
+        return execute_query(new_sql_query(query, str(e), 'openai'))
+        
+        
 
     rows = cur.fetchall()
 
